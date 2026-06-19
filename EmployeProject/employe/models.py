e5e20c23a352d79afdf361f20ce4f4f4e932d0f2
Elicar <anjaracharlinelicar@gmail.com>
2026-06-19 22:50:00 +0300

    Configuration du Tailwindcss et library DaisyUI

---
+++ EmployeProject/employe/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Employe(models.Model):
+    matricule     = models.CharField()
+    nom           = models.CharField(max_length=255)
+    email         = models.EmailField()
+    poste         = models.CharField(max_length=100)
+    salaire       = models.DecimalField(max_digits=10, decimal_places=2)
+    dateNaissance = models.DateField()
+    numero        = models.IntegerField()
+    
+    def __str__(self):
+        return self.nom
